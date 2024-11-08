2813fe5b05c1e30137b3fe19aed021bc973a1285
Yankey N'wollé Ange Christian <168791054+Yankeyange@users.noreply.github.com>
2024-11-08 12:56:06 +0000

    Add files via upload

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Topic
+from .forms import TopicForm
 
 from . import util
 
@@ -8,4 +10,21 @@ def index(request):
         "entries": util.list_entries()
     })
 
+# pour permettre à l'utilisateur de créer une nouvelle page
+def newpage(request):
+    # si la méthode est post
+    if request.method != 'POST':
+    # form va iriter de TopicForm
+     form = TopicForm()
+    # le sinon au cas où
+    else:
+        form = TopicForm(data=request.POST)
 
+    # maintenat si le formulaire est valide
+    if form.is_valid():
+      # si le formulaire est valide on sauvegarde
+      form.save()
+      # et on retourne à topics
+      return redirect('topics')
+    context = {'form':form}
+    return render(request, 'newpage.html', context)
